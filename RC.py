363cc7bf146d3239ef0a42cb3dce577da454899a
Sarah <sbreiff@gmail.com>
2015-02-27 16:16:25 -0800

    altered code slightly - ValueError now raised if non-nucleotide character is present in input.

--- RC.py
+++ RC.py
@@ -1,20 +1,19 @@
+def reverse_complement(seq):
 """
 Reverse complements a nucleotide sequence.
-Input: nucleotide sequence (string)
+Input: nucleotide sequence (string) - ambiguous base codes not allowed
 Output: reverse complement nucleotide sequence (string)
 """
-
-def reverse_complement(seq):
-	dict = {'A' : 'T', 'C' : 'G', 'G' : 'C', 'T' : 'A', 'a' : 't', 'c' : 'g',
+	rc_dict = {'A' : 'T', 'C' : 'G', 'G' : 'C', 'T' : 'A', 'a' : 't', 'c' : 'g',
 		'g' : 'c', 't' : 'a'}
 	rc = []
-	for i in range(len(seq)):
-		if seq[i] in dict:
-			rc.append(dict[seq[i]])
-		else: 
-			print "Input is not a proper nucleotide sequence."
-			return "Input is not a proper nucleotide sequence."
-	rc.reverse()
-	with open("rc_out.txt", 'w') as f:
-		f.write(''.join(rc))
-	return ''.join(rc)
+
+	try:
+                for i in range(len(seq)):
+			rc.append(rc_dict[seq[i]])
+                rc.reverse()
+                return ''.join(rc)
+
+        # Raise a ValueError if a non-nucleotide character is present in string
+        except KeyError:
+                raise ValueError("Input is not a proper nucleotide sequence.")

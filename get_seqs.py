6e2c67a90a5aaa6582a0cd5dda0be121664780de
Sarah <sbreiff@gmail.com>
2015-04-23 12:47:47 -0700

    edited get_seqs.py, program now also takes in a proteome fasta file as a second command line argument

--- get_seqs.py
+++ get_seqs.py
@@ -1,7 +1,7 @@
 '''
-Input: list of protein IDs
+Input: file containing list of protein IDs
 Output: fasta file containing all proteins specified in input file
-Command line usage: $ python get_seqs.py <input_file>
+Command line usage: $ python get_seqs.py <input_file> <proteome_file>
 '''
 
 from Bio import SeqIO
@@ -11,11 +11,10 @@ infile = argv[1]
 outfile = infile[:-4] + "_seqs.fasta"
 
 with open(infile, 'rb') as f:
-	seqlist = f.readlines()
-seqlist = [item.rstrip('\n') for item in seqlist]
+	seqlist = [item.rstrip('\n') for item in f.readlines()]
 
-proteome = list(SeqIO.parse("~/AllGeneModels_Translated.fasta",
-		"fasta"))
+with open(argv[2], 'r') as proteome_fasta:
+        proteome = list(SeqIO.parse(proteome_fasta, "fasta"))
 
 with open(outfile, 'w') as out_f:
 	for protein in proteome:

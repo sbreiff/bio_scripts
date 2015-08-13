0b597a64d9505dd80143fd631f208afdf087453b
Sarah <sbreiff@gmail.com>
2015-08-13 15:50:53 -0700

    Added comments to PCRLength.py

--- PCRLength.py
+++ PCRLength.py
@@ -19,25 +19,28 @@ from sys import argv
 from SeqTools import rev_comp
 import readline
 
-genes = list(SeqIO.parse(argv[1], "fasta"))
+genes = list(SeqIO.parse(argv[1], "fasta")) # parse fasta file
 
 while True:
-    gene_name = raw_input("\nGene ID: ")
-    if gene_name == "done":
+
+    gene_name = raw_input("\nGene ID: ") # prompt for gene name
+    if gene_name == "done": # signal for script to terminate
         break
-    for gene in genes:
+    for gene in genes: # load gene sequence into memory
         if gene.id == gene_name:
             seq = str(gene.seq)
+
+    # prompt for F primer
     f_primer = raw_input("Forward primer: ").upper().replace(' ', '')
-    if f_primer not in seq:
+    if f_primer not in seq: # notify user if F primer not in gene sequence
         print "Forward primer not present in DNA sequence."
         continue
+    # prompt for R primer
     r_primer = rev_comp(raw_input("Reverse primer: ").replace(' ', '')).upper()
-    if r_primer not in seq:
+    if r_primer not in seq: # notify user if R primer not in gene sequence
         print "Reverse primer not present in DNA sequence."
         continue
-    else:
+
+    else: # compute predicted length of PCR product
         length = seq.index(r_primer) - seq.index(f_primer) + len(r_primer)
-        # print seq.index(f_primer)
-        # print seq.index(r_primer)
         print "PCR Product Length: %d bp" % length

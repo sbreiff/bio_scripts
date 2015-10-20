7a57ceaed4836da42d4cd009ccccfaa6d334ec91
Sarah <sbreiff@gmail.com>
2015-10-19 17:38:29 -0700

    Added fasta_iterator to parse_fasta.py, a function that returns a generator rather than a list of Seq objects, so that it c an read file line by line and not load it all into memory at onc e, useful if file is very large.

--- fasta_parser.py
+++ fasta_parser.py
@@ -1,5 +1,7 @@
 '''
-A simple fasta parser
+Contains a couple versions of a fasta parser.
+
+1. parse_fasta: A simple fasta parser
 
 The parse_fasta function returns a list of Seq objects.
 Each Seq object contains the sequence id, the sequence description,
@@ -8,6 +10,25 @@ and the sequence.
 Example Usage (with example fasta file named 'samplefasta.fa'):
 >>> from fasta_parser import parse_fasta
 >>> records = parse_fasta('samplefasta.fa')
+>>> for record in records:
+...     print record.id
+...
+gene1
+gene2
+gene3
+>>>
+
+2. fasta_iterator: A fasta parser that creates a generator rather than loading
+all the results into memory. Useful if fasta file is very large.
+
+Used the same way as parse_fasta, except the contents can only be iterated
+through once.
+
+To be able to iterate through results more than once without creating the generator
+again, you can convert the contents to a list (which will load all results into
+memory at once):
+>>> from fasta_parser import fasta_iterator
+>>> records = list(fasta_iterator('samplefasta.fa'))
 
 For info on fasta format, see https://en.wikipedia.org/wiki/FASTA_format#Format
 
@@ -35,4 +56,20 @@ def parse_fasta(file):
     records.append(Seq(record))
     return records
             
-    
+def fasta_iterator(file):
+    fasta = open(file, 'r')
+    line = fasta.readline()
+    while True:
+        if line.startswith(">"):
+            text = line
+            line = fasta.readline()
+            while not line.startswith(">"):
+                text += line
+                line = fasta.readline()
+                if not line:
+                    break
+            yield Seq(text)
+        else:
+            line = fasta.readline()
+        if not line:
+            return
